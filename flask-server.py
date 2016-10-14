884f88d35d696d971432dad4d7dede99dfa8faef
Jimmy Hsu <jimmyhsu@live.com>
2016-10-14 13:46:35 -0700

    [update] add comments to flask server example

--- flask-server.py
+++ flask-server.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, render_template, json
 app = Flask(__name__)
 
+# Best for access speed; array requires us looping over O(n) while hash table is a single action O(1)
+# However, this isn't the best on space complexity as I'm duplicating the pet name entry
 pet_list = {}
 
+# Routes should also work for just /hello or /pets, but chrome append / bug makes this frustrating
 @app.route('/hello/', methods=['GET'])
 def hello():
     """
@@ -17,12 +20,14 @@ def pets():
     """
     if request.method == 'POST':
         json_pet = request.get_json()
+        # Data Validation
         if 'name' in json_pet and 'age' in json_pet and 'species' in json_pet:
             pet_list[json_pet['name']] = json_pet
             return json.jsonify(json_pet)
         else:
             return json.jsonify({'message': 'Missing: name, age, or species'}), 400
 
+    # Give full list of pets if just GET /pets
     return json.jsonify(pet_list)
 
 @app.route('/pets/<name>', methods=['GET'])
@@ -30,8 +35,6 @@ def get_pet(name):
     """
     Get Specific Pet
     """
-    print(pet_list)
-    print(name)
     if name in pet_list:
         return json.jsonify(pet_list[name])
     else:
