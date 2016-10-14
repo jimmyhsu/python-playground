2242d7516f2c363797f49fc88432f700052a5bcc
Jimmy Hsu <jimmyhsu@live.com>
2016-10-14 13:38:21 -0700

    [update] prior work; finished simple flask server

--- flask-server.py
+++ flask-server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, json
 app = Flask(__name__)
 
-pet_array = []
+pet_list = {}
 
 @app.route('/hello/', methods=['GET'])
 def hello():
@@ -17,14 +17,26 @@ def pets():
     """
     if request.method == 'POST':
         json_pet = request.get_json()
-        print(json_pet)
         if 'name' in json_pet and 'age' in json_pet and 'species' in json_pet:
-            pet_array.append(json_pet)
+            pet_list[json_pet['name']] = json_pet
             return json.jsonify(json_pet)
         else:
             return json.jsonify({'message': 'Missing: name, age, or species'}), 400
 
-    return jsonify(pet_array)
+    return json.jsonify(pet_list)
+
+@app.route('/pets/<name>', methods=['GET'])
+def get_pet(name):
+    """
+    Get Specific Pet
+    """
+    print(pet_list)
+    print(name)
+    if name in pet_list:
+        return json.jsonify(pet_list[name])
+    else:
+        return json.jsonify({'message': 'Can\'t find this specific pet' }), 404
+
 
 if __name__ == "__main__":
     app.run()
