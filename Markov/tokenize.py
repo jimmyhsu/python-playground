199f43c44d0ab8961c951ad2370b2a715ef377f6
Jimmy Hsu <jimmyhsu@live.com>
2016-12-14 17:04:31 -0800

    [add] markov

---
+++ Markov/tokenize.py
@@ -0,0 +1,31 @@
+import re
+import sys
+
+
+def tokenize(text):
+    no_punc_text = remove_punctuation(text)
+    tokens = split_on_whitespace(no_punc_text)
+    return tokens
+
+
+def remove_punctuation(text):
+    no_punc_text = re.sub('[()]', '', text)  # '[,.()]'
+    # no_punc_text = re.sub('--', ' ', no_punc_text)
+    return no_punc_text
+
+
+def split_on_whitespace(text):
+    return re.split('\s+', text)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        source = open(filename).read()
+        tokens = tokenize(source)
+        new_file = open("test.txt", "w")
+        new_file.write(' '.join(tokens))
+        new_file.close()
+        print(tokens)
+    else:
+        print('No source text filename given as argument')
